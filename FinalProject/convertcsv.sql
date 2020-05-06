6ca24ed0f7988e281fca4a7e09788ef42bb59194
James Bozhkov <36495009+jamesb97@users.noreply.github.com>
2020-05-06 14:16:10 -0400

    Add files via upload

---
+++ FinalProject/convertcsv.sql
@@ -0,0 +1,8 @@
+CREATE TABLE mytable(
+id INTEGER NOT NULL PRIMARY KEY
+,main VARCHAR(4) NOT NULL
+,description VARCHAR(20) NOT NULL
+,icon VARCHAR(3) NOT NULL
+
+INSERT INTO mytable(id, main, description, icon) VALUES (502, 'Rain', 'heavy intensity rain', '10d');
+INSERT INTO mytable(id, main, description, icon) VALUES (701, 'Mist', 'mist', '50d');
